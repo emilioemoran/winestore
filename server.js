52b9a5c8f363646686ca63a590c2b62b14c9b1d8
unknown <emilioemoran@gmail.com>
2022-12-01 17:15:47 -0300

    show products

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const hbs = require('hbs')
+const products = require("./data/wines.json")
+
+
+app.set('view engine', 'hbs')
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {})
 
 app.use(express.static('public'))
+
+
+console.log(products.products)
+app.get('/',function (req, res){
+  res.render('index',{
+    titulo: "Wine Store",
+    products : products.products
+  })
+})
+
+app.get('/login',function (req, res){
+  res.render('login',{  })
+})
+
+app.get('/about',function (req, res){
+  res.render('about',{
+    titulo: "Sobre nosotros"
+  })
+})
+
+app.get('/contact',function (req, res){
+  res.render('contact',{
+    titulo: "Contactanos"
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
